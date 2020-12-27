b6b320fe684f76527cc5d410fdd34ebaa2289eec
Mqasim0 <muhammadqasim3991@gmail.com>
2020-12-27 23:16:59 +0500

    Initial Author Routes

--- server.js
+++ server.js
@@ -2,19 +2,23 @@ if(process.env.NODE_ENV !== 'production'){
    require('dotenv').config()
 }
 
-const express = require('express')
+const express = require('express') // import express library
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 
-app.set('view engine', 'ejs')
-app.set('views', __dirname +  '/views')
-app.set('layout', 'layouts/layout')
-app.use(expressLayouts)
-app.use(express.static('public'))
+
+app.set('view engine', 'ejs') // set the view engine
+app.set('views', __dirname +  '/views')  // where views are coming from
+app.set('layout', 'layouts/layout') // layout files
+app.use(expressLayouts) // 
+app.use(express.static('public')) // public files images css etc
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
 
 // connect to the database
 
@@ -27,6 +31,7 @@ mongoose.connect(process.env.DATABASE_URL,{
  db.once('open ', ()=> console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 
 app.listen(process.env.PORT || 3000)
